ca934f4fd501f85e70e266fcd3f81b677e784781
Your Name <213172607@mycput.ac.za>
2021-06-04 20:05:45 +0200

    commit this Entity

---
+++ src/main/java/za/ac/cput/Entity/dogDescription.java
@@ -0,0 +1,69 @@
+package za.ac.cput.Entity;
+
+/*
+
+    Entity for the DogDescription
+    Author: Sinoxolo Jaca
+    Date: 04 June 2021
+ */
+
+public class dogDescription {
+
+    private int id;
+    private String type;
+
+    private dogDescription(Builder builder){
+
+        this.id = builder.id;
+        this.type = builder.type;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    @Override
+    public String toString() {
+        return "dogDescription{" +
+                "id=" + id +
+                ", type='" + type + '\'' +
+                '}';
+    }
+
+    public static class Builder {
+
+        private int id;
+        private String type;
+
+        public Builder setId(int id){
+
+            this.id = id;
+            return this;
+        }
+
+
+        public Builder setType(String type){
+            this.type = type;
+            return this;
+        }
+
+        public Builder copy(dogDescription desc){
+
+            this.id = desc.id;
+            this.type = desc.type;
+
+            return this;
+        }
+
+        public dogDescription build(){
+
+            return new dogDescription(this);
+        }
+
+
+    }
+}
