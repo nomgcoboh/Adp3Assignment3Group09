9b8978870b6e25dba81a30513d1bbba7ff4550ec
Jason Jaftha <jsnjaftha3@gmail.com>
2021-06-10 22:44:42 +0200

    Fifth Commit (Updated DogFactoryTest class)

--- src/test/java/za/ac/cput/Factory/DogFactoryTest.java
+++ src/test/java/za/ac/cput/Factory/DogFactoryTest.java
@@ -8,7 +8,9 @@ package za.ac.cput.Factory;
  */
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
 import za.ac.cput.Entity.Dog;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -25,10 +27,31 @@ class DogFactoryTest {
         dog2 = new DogFactory().createDog("Bimbis", 3, "Golden Brown", false, 'M', "Golden Retriever", 1001, "Vaccinated");
     }
 
+    @Timeout(5) //Timeout test
     @Test
     public void testCreateDog()
     {
         System.out.println(dog1.toString());
         System.out.println(dog2.toString());
     }
+
+    @Test
+    public void testEquality()
+    {
+        assertNotEquals(dog1, dog2);
+    }
+
+    @Test
+    public void testIdentity()
+    {
+        assertNotSame(dog1, dog2);
+    }
+
+    @Disabled("Test case not implemented!")
+    @Test
+    public void testBuilder()
+    {
+        System.out.println("...");
+    }
+
 }
