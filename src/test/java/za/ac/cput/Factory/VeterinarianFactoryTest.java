468196a9b042483be21958bad9c1ed9865eda5b8
Nonhlahla <nhlaa.nh@gmail.com>
2021-06-11 18:10:35 +0200

    Final Veterinarian Factory with test cases

--- src/test/java/za/ac/cput/Factory/VeterinarianFactoryTest.java
+++ src/test/java/za/ac/cput/Factory/VeterinarianFactoryTest.java
@@ -7,18 +7,51 @@ package za.ac.cput.Factory;
     Author: Nonhlahla Hlungwani (218160658)
     Date: 05 June 2021
  */
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import za.ac.cput.Entity.Veterinarian;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 
 class VeterinarianFactoryTest {
 
+   Veterinarian vet, vet1, vet2;
+
+    @BeforeEach
+   public void setUp() {
+
+        vet = new VeterinarianFactory().createVeterinarian("Nonhlahla", "Hlungwani", 0720657472, "218160658@mycput.ac.za");
+        vet1 = new VeterinarianFactory().createVeterinarian("Busisiwe", "Hlungwani", 0550657357, "234567890@mycput.ac.za");
+        vet2 = new VeterinarianFactory().createVeterinarian("Mandla", "Hlungwani", 277345632, "233767890@mycput.ac.za");
+
+
+    }
+    @Test
+    public void testVeterinarianEquality() {
+        System.out.println(vet.toString());
+        System.out.println(vet1.toString());
+        assertNotEquals(vet1, vet);
+    }
 
     @Test
-    public void createVeterinarian()
-    {
-        Veterinarian veterinarian = new VeterinarianFactory().createVeterinarian("Nonhlahla", "Hlungwani", 0720657472, "218160658@mycput.ac.za");
-        System.out.println(veterinarian);
+    void testVeterinarianIdentity() {
+        System.out.println("Vet  an Vet 2 are not the same");
+        assertNotSame(vet, vet1);
     }
+
+    @Test
+    @Timeout(1)
+    void testTimeout() throws InterruptedException {
+
+        System.out.println(vet2.toString());
+        assertNotEquals(vet1, vet2);
+    }
+
+    @Test
+    @Disabled
+    void disableVet2() {
+        System.out.println(vet2.toString());
+    }
+
 }
